0fb1fede5248a090f312df25e8f1633b488c7def
NLT <NamibiaLT@users.noreply.github.com>
2019-02-22 12:26:10 -0800

    open/ read csv file

--- csv_normalizer.py
+++ csv_normalizer.py
@@ -1,1 +1,15 @@
+import pandas as pd
+import datetime
+import time
+import pytz
+
+
+a_file = 'sample.csv'
+
+def open_and_read_csv(a_file):
+	data_frame = pd.read_csv(a_file, sep= ',', na_values=['None', 'none'])
+
+	return data_frame
+
+data_frame = open_and_read_csv(a_file)
 
