47ed44ea74c148bee61dc9a53e6fc93f31f615ec
NLT <NamibiaLT@users.noreply.github.com>
2019-02-22 13:48:10 -0800

    add parse address test

--- test_csv_normalizer.py
+++ test_csv_normalizer.py
@@ -5,6 +5,8 @@ broken_sample_file = 'sample-with-broken-utf8.csv'
 zipcode = 1
 fullname = 'Monkey Alberto'
 timestamp = '10/20/19 1:00:00 AM'
+address = "100 Hoover Dr., Sampletown, NH"
+no_commas_address = "Hey there this is a town"
 
 def test_read_stdin_as_dataframe():
 	assert csvn.read_stdin_as_dataframe() == data_frame
@@ -17,3 +19,7 @@ def test_format_zipcodes():
 
 def test_convert_names_colmn_to_uppercase():
 	assert csvn.convert_names_colmn_to_uppercase(fullname) == 'MONKEY ALBERTO'
+
+def test_parse_address():
+	assert csvn.parse_address(address) == "100 Hoover Dr., Sampletown, NH"
+	assert csvn.parse_address(no_commas_address) == "Hey there this is a town"
