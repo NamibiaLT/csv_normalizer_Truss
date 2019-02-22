d666b7e48ad066f62a4eca498e4cd373c1f5e47a
NLT <NamibiaLT@users.noreply.github.com>
2019-02-22 12:51:09 -0800

    add new stdin function

--- csv_normalizer.py
+++ csv_normalizer.py
@@ -1,17 +1,28 @@
 import pandas as pd
 from datetime import datetime
-import time
 from pytz import timezone
+import io
+import sys
 
 
 a_file = 'sample.csv'
 
-def open_and_read_csv(a_file):
-	data_frame = pd.read_csv(a_file, sep= ',', na_values=['None', 'none'])
+def read_stdin_as_dataframe():
+	'''
+	Invalid characters will be replaced with the Unicode Replacement
+	Character.
+	If that replacement makes data invalid (for example,
+	because it turns a date field into something unparseable),
+	a warning will be printed to `stderr`
+	and the row will be dropped from output
+
+	Pandas is used because it handles address commas
+	'''
+	raw_data = sys.stdin.detach().read()
+	decoded_data = raw_data.decode('utf-8', 'replace')
+	data_frame = pd.read_csv(io.StringIO(decoded_data), encoding='utf-8')
 
 	return data_frame
-
-data_frame = open_and_read_csv(a_file)
 
 def parse_datetime(data_frame):
 	'''
