4a15de35f0a29662edc868bbceab2f0aa5c16d57
NLT <NamibiaLT@users.noreply.github.com>
2019-02-22 13:42:42 -0800

    update tests

--- test_csv_normalizer.py
+++ test_csv_normalizer.py
@@ -2,15 +2,18 @@ import csv_normalizer as csvn
 
 sample_file = 'sample.csv'
 broken_sample_file = 'sample-with-broken-utf8.csv'
+zipcode = 1
+fullname = 'Monkey Alberto'
+timestamp = '10/20/19 1:00:00 AM'
 
 def test_read_stdin_as_dataframe():
-	assert csvn.read_stdin_as_dataframe(broken_sample_file) == data_frame
+	assert csvn.read_stdin_as_dataframe() == data_frame
 
 def test_parse_timestamp():
-		assert csvn.parse_datetime(sample_file) == timestamp_iso
+		assert csvn.parse_timestamp(timestamp) == '2019-10-20T04:00:00-04:00'
 
 def test_format_zipcodes():
-	assert csvn.format_zipcodes(sample_file)[1] == '00001'
+	assert csvn.format_zipcodes(zipcode) == '00001'
 
 def test_convert_names_colmn_to_uppercase():
-	assert csvn.convert_names_colmn_to_uppercase(sample_file)[0] == 'MONKEY ALBERTO'
+	assert csvn.convert_names_colmn_to_uppercase(fullname) == 'MONKEY ALBERTO'
